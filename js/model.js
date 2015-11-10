337b8812d9d3724773d2121af2e9524e7a43b1b7
saashimi <saavedra.kevin@gmail.com>
2015-11-10 10:14:37 -0800

    KS 1st attempt at model object.

---
+++ js/model.js
@@ -0,0 +1,57 @@
+; //defensive semicolon
+
+function ModelWrapper() {
+  
+  "use strict";
+  var model = {}; // model object lies outside of the methods.
+ 
+  var Model = {
+
+    getModel : function() { 
+     
+      $(document).ready(function() {
+        var url = "https://spreadsheets.google.com/";
+        var feedType = "feeds/cells/";
+        var key = "1sYkU_8raV14Bqm33dWcaksC3iMm73DH9OMsooxSMItM/";
+        var sheetID = "od6/";
+        var etc = "public/values?alt=json"
+
+        $.getJSON(url + feedType + key + sheetID + etc, function(data) {
+          //var model = {}; //originally an array, now an object.
+          var col_high = 0;
+
+          $.each(data.feed.entry, function(key, value) {
+            var cur_col = Number(value.gs$cell.col);  //Returns column value
+            if (cur_col > col_high) {
+              col_high = cur_col; //reset the value as we iterate
+              model[(cur_col).toString()] = {};
+              }
+              model[value.gs$cell.col][value.gs$cell.row] = value.gs$cell.$t;
+            }); // End .each
+            console.log("inside = ", model);
+          }); // End .getJSON
+          console.log("outer = ", model);
+      }); // End JQuery wrapper
+    
+    }, // End getModel()
+
+    setModel : function(num_val, alpha_val, greet_val) { // input parameters
+
+      var data = {
+        "Num" : num_val,
+        "Alpha" : alpha_val,
+        "Greet" : greet_val
+      };
+      var address = "https://sheetsu.com/apis/0a299348";
+      $.post(address, data);
+    }
+
+  }; //End Model
+
+
+  Model.getModel();
+  console.log(model);
+  Model.setModel("100", "z", "Test2");
+
+} // End ModelWrapper
+ModelWrapper();
