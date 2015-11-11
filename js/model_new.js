9a4f930255ff4a16a9b86fe31f7b55fdd11bd357
saashimi <saavedra.kevin@gmail.com>
2015-11-11 10:34:23 -0800

    cleaned up after a few tests.

--- js/model_new.js
+++ js/model_new.js
@@ -1,12 +1,15 @@
 ; // defensive semicolon
+//controller updates information by calling model.data = {some : info} 
 
-var model = {
+var model = { // This is the object
     url: "https://spreadsheets.google.com/feeds/cells/" +
           "1sYkU_8raV14Bqm33dWcaksC3iMm73DH9OMsooxSMItM" +
           "/od6/public/values?alt=json",
     data: {},
 
     getData: function(callback) {
+    //Extracts spreadsheet data using the Google Sheets API. Objects are formatted
+    //as {Column {Row : Value}} to facilitate 
         $.getJSON(model.url, function(data) {
             var col_high = 0;
 
@@ -17,11 +20,11 @@ var model = {
                     model.data[(cur_col).toString()] = {};
                 }
                 model.data[value.gs$cell.col][value.gs$cell.row] = value.gs$cell.$t;
-            }); // End .each
-            callback(model.data);
+            });
+            callback(model.data); // KS added return. This is the outgoing
+                                        // data.
         })
-    },
-
+    }, 
 
     setModel: function(data) {
         var data = {};
@@ -30,19 +33,21 @@ var model = {
         })
         var address = "https://sheetsu.com/apis/0a299348";
         $.post(address, data);
-    },
-
-    updateModel: function() {
-
+    }, // End setModel
+ 
+    updateModel: function(data) {
+        model.getData();
     }
 }
 
+//....End Model Object....//
 
 function displayCallback(data) {
     console.log(data);
-}
-
-$(document).ready(function() {
+} // end displayCallback
+        
+$(document).ready(function() { 
     model.getData(displayCallback);
+    //console.log(model.updateModel());
 });
 
